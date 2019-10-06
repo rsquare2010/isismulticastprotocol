c94990ccbfbf5c91970341a44eafa760d31d18b1
rsquare2010 <rahulceg1092@gmail.com>
2019-10-06 02:08:03 -0400

    support for multiprocess

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,5 @@ docker stop udptestsend
 docker rm udptestsend
 docker stop udptestrecv
 docker rm udptestrecv
+docker stop udptest1
+docker rm udptest1
--- src/udp-send1.c
+++ src/udp-send1.c
@@ -15,6 +15,7 @@
 #define MSGS 5    /* number of messages to send */
 #define PORT 21233
 #define MAX_STRING_SIZE 40
+#define MAX_PEERS 20
 
 char pnames[3][MAX_STRING_SIZE];
 int psize;
@@ -49,16 +50,18 @@ void readFromHostFile( char* myIP) {
     host_entry = gethostbyname(temp);
     if(host_entry != NULL) {
         IPbuffer1 = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
-        printf("size of pnames to be assigned is:%d",host_entry->h_length);
     }
+//    printf("IPB:%s",IPbuffer1);
     int index = 0;
     while (temp != NULL)
     {
-        if(strcmp(IPbuffer1, myIPAddress)!=0) {
+        printf("IPB:%s",IPbuffer1);
+        if((IPbuffer1 != NULL) && strcmp(IPbuffer1, myIPAddress)!=0) {
             strcpy(pnames[index], IPbuffer1);
             index++;
         }
         temp = strtok (NULL, "\n");
+        IPbuffer1 = NULL;
         if( temp != NULL) {
             host_entry = gethostbyname(temp);
             if(host_entry != NULL) {
@@ -67,7 +70,7 @@ void readFromHostFile( char* myIP) {
         }
     }
     for(int i = 0; i< index; i++) {
-        printf("The tokens are: %s",pnames[i]);
+        printf("The tokens are: %s\n",pnames[i]);
     }
     psize = index;
 
@@ -76,16 +79,18 @@ void readFromHostFile( char* myIP) {
 int main(int argc, char **argv)
 {
     int option = 0;
-    int counter;
+    int MSG_COUNT = 0;
+    int messageCounter = 0;
+    int sequnceCounter = 0;
     char* path;
+    int myId;
     
-    struct sockaddr_in myaddr, servaddr;
+    struct sockaddr_in myaddr, servaddr[MAX_PEERS];
     struct sockaddr_in remaddr;
     socklen_t addrlen = sizeof(remaddr);
     int fd, i, slen=sizeof(servaddr);
     unsigned char buf[BUFLEN];    /* message buffer */
     int recvlen;        /* # bytes in acknowledgement message */
-    char *host = "udptestsend";
     struct hostent *hp;
     
     char *filecontents;
@@ -93,11 +98,6 @@ int main(int argc, char **argv)
     
     AckMessage ackMessages;
     SeqMessage seqMessage;
-    ackMessages.type = 2;
-    ackMessages.sender = 2001;
-    ackMessages.msg_id = 9992;
-    ackMessages.proposed_seq = 2234;
-    ackMessages.proposer = 9191919;
     DataMessage sendMessage = {1, 1002, 9991, 123};
     DataMessage rcvdDMMessage;
     AckMessage rcvdAMMdessage;
@@ -109,7 +109,7 @@ int main(int argc, char **argv)
                 path = optarg;
                 break;
             case 'c' :
-                counter =  atoi(optarg);
+                MSG_COUNT =  atoi(optarg);
                 break;
             default: //print_usage();
                 exit(EXIT_FAILURE);
@@ -121,6 +121,8 @@ int main(int argc, char **argv)
     if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1)
         printf("socket created\n");
     
+    int broadcastEnable=1;
+    int ret=setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
     
     /* bind it to all local addresses and pick any port number */
     
@@ -134,77 +136,78 @@ int main(int argc, char **argv)
         perror("bind failed");
         return 0;
     }
-    printf("bind succeded\n");
     /* now define servaddr, the address to whom we want to send messages */
     /* For convenience, the host address is expressed as a numeric IP address */
     /* that we will convert to a binary format via inet_aton */
     
-    memset((char *) &servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERVICE_PORT);
     sleep(5);
-    do {
-        printf("host is:%s", host);
-        hp = gethostbyname(host);
-        if (hp == NULL) {
-            fprintf(stderr, "could not obtain address of %s\n", host);
-            sleep(3);
-            //            return 0;
-        }
-        else {
-            char *IPbuffer = inet_ntoa(*((struct in_addr*)
-                                   hp->h_addr_list[0]));
-            printf("connected to :%s", IPbuffer);
-            break;
-        }
-    } while(1 == 1);
-    printf("usnure if host connected?\n");
+
     /* put the host's address into the server address structure */
     
     char hostbuffer[256];
     char *IPbuffer;
     struct hostent *host_entry;
     int hostname = gethostname(hostbuffer, sizeof(hostbuffer));
+    myId = hostbuffer[0] + hostbuffer [1] + hostbuffer[3] + hostbuffer[4];
+    
     host_entry = gethostbyname(hostbuffer);
     IPbuffer = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
 
+    printf("my ip is: %s\n", IPbuffer);
     readFromHostFile(IPbuffer);
 
+    for (int i = 0; i < psize; i++) {
+        memset((char *) &servaddr[i], 0, sizeof(servaddr[i]));
+        servaddr[i].sin_family = AF_INET;
+        servaddr[i].sin_port = htons(SERVICE_PORT);
+        servaddr[i].sin_addr.s_addr = inet_addr(pnames[i]);
+    }
     
-    printf("address in token is %s\n", pnames[0]);
-    printf("size of pnames to be assigned is:%ld",strlen(pnames[0]));
-
-    servaddr.sin_addr.s_addr = inet_addr(pnames[0]);
     
-    serializeDM(&sendMessage, &buf[0]);
+    
 
     /* now let's send the messages */
-    printf("before send\n");
 
+    
     for (i=0; i < MSGS; i++) {
-//            printf("sending Messages %ld", sizeof(messageSerialized));
-            
-            if (sendto(fd, buf, sizeof(sendMessage), 0, (struct sockaddr *)&servaddr, sizeof(struct sockaddr_in))==-1) {
+        for (int pid = 0; pid < psize; pid ++) {
+            sendMessage.message_id = messageCounter;
+            sendMessage.sender = myId;
+            serializeDM(&sendMessage, &buf[0]);
+            if (sendto(fd, buf, sizeof(sendMessage), 0, (struct sockaddr *)&servaddr[pid], sizeof(struct sockaddr_in))==-1) {
                 perror("sendto failed");
             }
         }
+        messageCounter++;
+    }
         
         for (;;) {
             
-            recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&servaddr, &slen);
+            recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &addrlen);
             if (recvlen > 0) {
                             int temp;
                             memcpy(&temp, &buf[0], 4);
                 if(ntohl(temp) == 1) {
                     deserializeDM(buf, &rcvdDMMessage);
-                    printf("Data message received \n");
+                    ackMessages.type = 2;
+                    ackMessages.sender = rcvdDMMessage.sender;
+                    ackMessages.msg_id = rcvdDMMessage.message_id;
+                    ackMessages.proposed_seq = sequnceCounter;
+                    ackMessages.proposer = myId;
+                    sequnceCounter++;
+                    
                     serializeAM(&ackMessages, &buf[0]);
-                    if (sendto(fd, buf, sizeof(ackMessages), 0, (struct sockaddr *)&servaddr, sizeof(struct sockaddr_in))==-1) {
+                    printf("id  of received message: %d\n",rcvdDMMessage.message_id);
+                    printf("received message sender:%d\n", rcvdDMMessage.sender);
+                    
+                    int len=50;
+                    char ipbuffer[len];
+                    if (sendto(fd, buf, sizeof(ackMessages), 0, (struct sockaddr *)&remaddr, sizeof(struct sockaddr_in))==-1) {
                         perror("sendto failed");
                     }
                 } else if (ntohl(temp) == 2) {
                     deserializeAM(buf, &rcvdAMMdessage);
-                    printf("Ack received\n");
+                    printf("Ack received%d\n",rcvdAMMdessage.msg_id);
                 }
             }
             else
